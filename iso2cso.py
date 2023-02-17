274084b3b09db61e06ce46aaba2dbd50ca86f14c
bahamutyim <bahamutyim@gmail.com>
2023-02-17 16:47:58 +0800

    Add files via upload

---
+++ iso2cso.py
@@ -0,0 +1,30 @@
+import os
+
+dir_path = '.'
+
+files =[]
+dirs = []
+
+def list_directory(_dir_path, _files):
+    for path in os.listdir(_dir_path):
+        fullpath = os.path.join(_dir_path, path)
+        print(fullpath)
+        if path[-4:] == ".iso" and os.path.isfile(os.path.join(fullpath)):
+            #_files.append(fullpath)
+            print("compress " + fullpath)
+            os.rename(fullpath,".\\temp.iso")
+            os.system("ciso.exe 9 temp.iso temp.cso")
+            os.rename(".\\temp.cso", fullpath[:-4] + ".cso")
+            os.remove(".\\temp.iso")
+        if os.path.isdir(fullpath):
+            list_directory(fullpath, _files)
+
+
+list_directory(dir_path, files)
+
+print("files:")        
+print(files)
+print("directory:")
+print(dirs)
+
+
