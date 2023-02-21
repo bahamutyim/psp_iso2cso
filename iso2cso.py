88f9abac279d96a9828b7eff830452abc4479300
raymond yim <raymond.yim@tfs-game.com>
2023-02-21 15:48:34 +0800

    add 1 second to sleep to prevent temp.iso cannot be delete change extension change to be lower case

--- iso2cso.py
+++ iso2cso.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 dir_path = '.'
 
@@ -9,12 +10,13 @@ def list_directory(_dir_path, _files):
     for path in os.listdir(_dir_path):
         fullpath = os.path.join(_dir_path, path)
         print(fullpath)
-        if path[-4:] == ".iso" and os.path.isfile(os.path.join(fullpath)):
+        if path[-4:].lower() == ".iso" and os.path.isfile(os.path.join(fullpath)):
             #_files.append(fullpath)
             print("compress " + fullpath)
             os.rename(fullpath,".\\temp.iso")
             os.system("ciso.exe 9 temp.iso temp.cso")
             os.rename(".\\temp.cso", fullpath[:-4] + ".cso")
+            time.sleep(1)
             os.remove(".\\temp.iso")
         if os.path.isdir(fullpath):
             list_directory(fullpath, _files)
