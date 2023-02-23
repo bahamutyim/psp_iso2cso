756b4feee801be18f694c6e5e02bec10149b5391
raymond yim <raymond.yim@tfs-game.com>
2023-02-23 17:45:27 +0800

    remove unuse code

--- iso2cso.py
+++ iso2cso.py
@@ -3,15 +3,13 @@ import time
 
 dir_path = '.'
 
-files =[]
-dirs = []
 
-def list_directory(_dir_path, _files):
+
+def list_directory(_dir_path):
     for path in os.listdir(_dir_path):
         fullpath = os.path.join(_dir_path, path)
-        print(fullpath)
+        #print(fullpath)
         if path[-4:].lower() == ".iso" and os.path.isfile(os.path.join(fullpath)):
-            #_files.append(fullpath)
             print("compress " + fullpath)
             os.rename(fullpath,".\\temp.iso")
             os.system("ciso.exe 9 temp.iso temp.cso")
@@ -19,14 +17,10 @@ def list_directory(_dir_path, _files):
             time.sleep(1)
             os.remove(".\\temp.iso")
         if os.path.isdir(fullpath):
-            list_directory(fullpath, _files)
+            list_directory(fullpath)
 
 
-list_directory(dir_path, files)
+list_directory(dir_path)
 
-print("files:")        
-print(files)
-print("directory:")
-print(dirs)
 
 
